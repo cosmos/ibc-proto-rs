74ce51022f765f2676a0f5e77b4d62b9a1def784
Farhad Shabani <farhad.shabani@gmail.com>
2023-06-01 09:58:00 -0700

    Include missing paths through type_attribute() to apply serde, and rebuild

--- src/prost/ibc.applications.interchain_accounts.v1.rs
+++ src/prost/ibc.applications.interchain_accounts.v1.rs
@@ -1,4 +1,5 @@
 /// An InterchainAccount is defined as a BaseAccount & the address of the account owner on the controller chain
+#[cfg_attr(feature = "std", derive(::serde::Serialize, ::serde::Deserialize))]
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct InterchainAccount {
@@ -22,6 +23,7 @@ pub struct InterchainAccountPacketData {
     pub memo: ::prost::alloc::string::String,
 }
 /// CosmosTx contains a list of sdk.Msg's. It should be used when sending transactions to an SDK host chain.
+#[cfg_attr(feature = "std", derive(::serde::Serialize, ::serde::Deserialize))]
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CosmosTx {
@@ -32,6 +34,7 @@ pub struct CosmosTx {
 }
 /// Type defines a classification of message issued from a controller chain to its associated interchain accounts
 /// host
+#[cfg_attr(feature = "std", derive(::serde::Serialize, ::serde::Deserialize))]
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 pub enum Type {
@@ -62,6 +65,7 @@ impl Type {
 }
 /// Metadata defines a set of protocol specific data encoded into the ICS27 channel version bytestring
 /// See ICS004: <https://github.com/cosmos/ibc/tree/master/spec/core/ics-004-channel-and-packet-semantics#Versioning>
+#[cfg_attr(feature = "std", derive(::serde::Serialize, ::serde::Deserialize))]
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Metadata {
