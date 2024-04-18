54eb4d68aff0224e589bbf26d7facd04be088c91
Carlos Rodriguez <carlos@interchain.io>
2024-04-18 15:51:58 +0200

    update rollkit ibc commit

--- src/prost/ibc.lightclients.rollkit.v1.rs
+++ src/prost/ibc.lightclients.rollkit.v1.rs
@@ -1,3 +1,40 @@
+/// ClientState defines a Rollkit rollup client that tracks the
+/// consensus state of the counterparty rollup.
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ClientState {
+    #[prost(message, optional, tag = "1")]
+    pub tendermint_client_state: ::core::option::Option<
+        super::super::tendermint::v1::ClientState,
+    >,
+    #[prost(message, optional, tag = "2")]
+    pub da_params: ::core::option::Option<DaParams>,
+}
+impl ::prost::Name for ClientState {
+    const NAME: &'static str = "ClientState";
+    const PACKAGE: &'static str = "ibc.lightclients.rollkit.v1";
+    fn full_name() -> ::prost::alloc::string::String {
+        ::prost::alloc::format!("ibc.lightclients.rollkit.v1.{}", Self::NAME)
+    }
+}
+/// DaParams defines the data-availability related parameters of a Rollkit rollup light.
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct DaParams {
+    /// client ID of the DA light client
+    #[prost(string, tag = "1")]
+    pub client_id: ::prost::alloc::string::String,
+    /// fraud dispute period window in nanoseconds
+    #[prost(uint64, tag = "2")]
+    pub fraud_period_window: u64,
+}
+impl ::prost::Name for DaParams {
+    const NAME: &'static str = "DaParams";
+    const PACKAGE: &'static str = "ibc.lightclients.rollkit.v1";
+    fn full_name() -> ::prost::alloc::string::String {
+        ::prost::alloc::format!("ibc.lightclients.rollkit.v1.{}", Self::NAME)
+    }
+}
 /// Header defines the structure of the header for Rollkit light clients
 /// operating on a Data Availability layer. It encapsulates all the information
 /// necessary to update a client from a trusted Rollkit rollup ConsensusState.
@@ -27,9 +64,6 @@ impl ::prost::Name for Header {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct DaData {
-    /// client ID of the DA light client
-    #[prost(string, tag = "1")]
-    pub client_id: ::prost::alloc::string::String,
     /// the proof of inclusion of Rollkit rollup block data in DA block
     #[prost(bytes = "vec", tag = "2")]
     pub shared_proof: ::prost::alloc::vec::Vec<u8>,
--- src/prost/ibc.lightclients.rollkit.v1.serde.rs
+++ src/prost/ibc.lightclients.rollkit.v1.serde.rs
@@ -1,4 +1,4 @@
-impl serde::Serialize for DaData {
+impl serde::Serialize for ClientState {
     #[allow(deprecated)]
     fn serialize<S>(&self, serializer: S) -> core::result::Result<S::Ok, S::Error>
     where
@@ -12,10 +12,114 @@ impl serde::Serialize for DaData {
         if true {
             len += 1;
         }
-        let mut struct_ser = serializer.serialize_struct("ibc.lightclients.rollkit.v1.DaData", len)?;
-        if true {
-            struct_ser.serialize_field("clientId", &self.client_id)?;
+        let mut struct_ser = serializer.serialize_struct("ibc.lightclients.rollkit.v1.ClientState", len)?;
+        if let Some(v) = self.tendermint_client_state.as_ref() {
+            struct_ser.serialize_field("tendermintClientState", v)?;
         }
+        if let Some(v) = self.da_params.as_ref() {
+            struct_ser.serialize_field("daParams", v)?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for ClientState {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> core::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "tendermint_client_state",
+            "tendermintClientState",
+            "da_params",
+            "daParams",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            TendermintClientState,
+            DaParams,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> core::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> core::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "tendermintClientState" | "tendermint_client_state" => Ok(GeneratedField::TendermintClientState),
+                            "daParams" | "da_params" => Ok(GeneratedField::DaParams),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = ClientState;
+
+            fn expecting(&self, formatter: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                formatter.write_str("struct ibc.lightclients.rollkit.v1.ClientState")
+            }
+
+            fn visit_map<V>(self, mut map_: V) -> core::result::Result<ClientState, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut tendermint_client_state__ = None;
+                let mut da_params__ = None;
+                while let Some(k) = map_.next_key()? {
+                    match k {
+                        GeneratedField::TendermintClientState => {
+                            if tendermint_client_state__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("tendermintClientState"));
+                            }
+                            tendermint_client_state__ = map_.next_value()?;
+                        }
+                        GeneratedField::DaParams => {
+                            if da_params__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("daParams"));
+                            }
+                            da_params__ = map_.next_value()?;
+                        }
+                    }
+                }
+                Ok(ClientState {
+                    tendermint_client_state: tendermint_client_state__,
+                    da_params: da_params__,
+                })
+            }
+        }
+        deserializer.deserialize_struct("ibc.lightclients.rollkit.v1.ClientState", FIELDS, GeneratedVisitor)
+    }
+}
+impl serde::Serialize for DaData {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> core::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if true {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("ibc.lightclients.rollkit.v1.DaData", len)?;
         if true {
             #[allow(clippy::needless_borrow)]
             struct_ser.serialize_field("sharedProof", pbjson::private::base64::encode(&self.shared_proof).as_str())?;
@@ -30,15 +134,12 @@ impl<'de> serde::Deserialize<'de> for DaData {
         D: serde::Deserializer<'de>,
     {
         const FIELDS: &[&str] = &[
-            "client_id",
-            "clientId",
             "shared_proof",
             "sharedProof",
         ];
 
         #[allow(clippy::enum_variant_names)]
         enum GeneratedField {
-            ClientId,
             SharedProof,
         }
         impl<'de> serde::Deserialize<'de> for GeneratedField {
@@ -61,7 +162,6 @@ impl<'de> serde::Deserialize<'de> for DaData {
                         E: serde::de::Error,
                     {
                         match value {
-                            "clientId" | "client_id" => Ok(GeneratedField::ClientId),
                             "sharedProof" | "shared_proof" => Ok(GeneratedField::SharedProof),
                             _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
                         }
@@ -82,16 +182,9 @@ impl<'de> serde::Deserialize<'de> for DaData {
                 where
                     V: serde::de::MapAccess<'de>,
             {
-                let mut client_id__ = None;
                 let mut shared_proof__ = None;
                 while let Some(k) = map_.next_key()? {
                     match k {
-                        GeneratedField::ClientId => {
-                            if client_id__.is_some() {
-                                return Err(serde::de::Error::duplicate_field("clientId"));
-                            }
-                            client_id__ = Some(map_.next_value()?);
-                        }
                         GeneratedField::SharedProof => {
                             if shared_proof__.is_some() {
                                 return Err(serde::de::Error::duplicate_field("sharedProof"));
@@ -103,12 +196,124 @@ impl<'de> serde::Deserialize<'de> for DaData {
                     }
                 }
                 Ok(DaData {
-                    client_id: client_id__.unwrap_or_default(),
                     shared_proof: shared_proof__.unwrap_or_default(),
                 })
             }
         }
         deserializer.deserialize_struct("ibc.lightclients.rollkit.v1.DaData", FIELDS, GeneratedVisitor)
+    }
+}
+impl serde::Serialize for DaParams {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> core::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if true {
+            len += 1;
+        }
+        if true {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("ibc.lightclients.rollkit.v1.DaParams", len)?;
+        if true {
+            struct_ser.serialize_field("clientId", &self.client_id)?;
+        }
+        if true {
+            #[allow(clippy::needless_borrow)]
+            struct_ser.serialize_field("fraudPeriodWindow", ::alloc::string::ToString::to_string(&self.fraud_period_window).as_str())?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for DaParams {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> core::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "client_id",
+            "clientId",
+            "fraud_period_window",
+            "fraudPeriodWindow",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            ClientId,
+            FraudPeriodWindow,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> core::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> core::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "clientId" | "client_id" => Ok(GeneratedField::ClientId),
+                            "fraudPeriodWindow" | "fraud_period_window" => Ok(GeneratedField::FraudPeriodWindow),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = DaParams;
+
+            fn expecting(&self, formatter: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                formatter.write_str("struct ibc.lightclients.rollkit.v1.DaParams")
+            }
+
+            fn visit_map<V>(self, mut map_: V) -> core::result::Result<DaParams, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut client_id__ = None;
+                let mut fraud_period_window__ = None;
+                while let Some(k) = map_.next_key()? {
+                    match k {
+                        GeneratedField::ClientId => {
+                            if client_id__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("clientId"));
+                            }
+                            client_id__ = Some(map_.next_value()?);
+                        }
+                        GeneratedField::FraudPeriodWindow => {
+                            if fraud_period_window__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("fraudPeriodWindow"));
+                            }
+                            fraud_period_window__ = 
+                                Some(map_.next_value::<::pbjson::private::NumberDeserialize<_>>()?.0)
+                            ;
+                        }
+                    }
+                }
+                Ok(DaParams {
+                    client_id: client_id__.unwrap_or_default(),
+                    fraud_period_window: fraud_period_window__.unwrap_or_default(),
+                })
+            }
+        }
+        deserializer.deserialize_struct("ibc.lightclients.rollkit.v1.DaParams", FIELDS, GeneratedVisitor)
     }
 }
 impl serde::Serialize for Header {
