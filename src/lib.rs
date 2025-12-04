b1bd5594fedd999c023181ccbd3000a43ad0f19a
srdtrk <srdtrk@hotmail.com>
2025-12-04 14:51:51 +0400

    imp: wire attestations

--- src/lib.rs
+++ src/lib.rs
@@ -164,6 +164,13 @@ pub mod ibc {
                 include_proto!("ibc.lightclients.wasm.v1.serde.rs");
             }
         }
+        pub mod attestations {
+            pub mod v1 {
+                include_proto!("ibc.lightclients.attestations.v1.rs");
+                #[cfg(feature = "serde")]
+                include_proto!("ibc.lightclients.attestations.v1.serde.rs");
+            }
+        }
     }
     pub mod mock {
         include_proto!("ibc.mock.rs");
